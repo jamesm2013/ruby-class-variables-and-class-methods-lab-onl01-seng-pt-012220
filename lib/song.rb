c05bbd6ae9d319c21cf526a3f7aa5579585fd480
james Johnson <2013matthew2013@gmail.com>
2020-02-10 00:45:43 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+ 
+ def initialize(song_name,artist,genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end  
+end  
