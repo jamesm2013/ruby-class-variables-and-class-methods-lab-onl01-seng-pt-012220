ff263bb9bdc18e34abda5f97f70dfcffc1e8d0f9
james Johnson <2013matthew2013@gmail.com>
2020-02-10 00:52:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,5 +22,9 @@ class Song
  def self.genres
     @@genres.uniq
   end 
-
+def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    artist_count
+  end
 end  
